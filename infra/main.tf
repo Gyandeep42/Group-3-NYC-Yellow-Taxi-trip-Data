8574188d97d88d7aeb3b4d25d8c5092d0250b0a5
Gyandeep42 <yadavgyan2002@email.com>
2025-08-07 01:22:15 +0530

    from scrach commit

--- infra/main.tf
+++ infra/main.tf
@@ -2,46 +2,53 @@ provider "aws" {
   region = "us-east-1"
 }
 
+data "aws_caller_identity" "current" {}
+
 locals {
-  glue_role_arn = "arn:aws:iam::963702399712:role/labrole"  # Replace with actual
+  glue_role_name       = "AWSGlueServiceRoleDefault"
+  glue_role_arn        = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${local.glue_role_name}"
+  script_s3_path       = "s3://first-glue-bkt-grp-three-nyc/scripts/etl-glue-script.py"
+  raw_data_s3_path     = "s3://first-glue-bkt-grp-three-nyc/raw_data/"
+  cleaned_data_s3_path = "s3://try-ci-cd-bkt-cleanned/cleaned_data/"
 }
 
-resource "aws_glue_catalog_database" "etl_db" {
-  name = "nyc_yellow_taxi_etl_db"
+resource "aws_glue_catalog_database" "nyc_db" {
+  name = "nyc_yellow_trip_db"
 }
 
-resource "aws_glue_job" "etl_job" {
-  name     = var.glue_job_name
+resource "aws_glue_job" "nyc_etl_job" {
+  name     = "nyc-yellow-taxi-etl-job"
   role_arn = local.glue_role_arn
 
   command {
     name            = "glueetl"
-    script_location = var.script_s3_path
+    script_location = local.script_s3_path
     python_version  = "3"
+  }
+
+  default_arguments = {
+    "--TempDir"         = "${local.cleaned_data_s3_path}temp/"
+    "--job-language"    = "python"
+    "--raw_bucket"      = local.raw_data_s3_path
+    "--cleaned_bucket"  = local.cleaned_data_s3_path
   }
 
   glue_version      = "4.0"
   number_of_workers = 2
   worker_type       = "G.1X"
-
-  default_arguments = {
-    "--job-language"                      = "python"
-    "--TempDir"                           = "s3://${var.bucket_name_prefix}/temp/"
-    "--enable-continuous-cloudwatch-log" = "true"
-    "--enable-metrics"                   = "true"
+  execution_property {
+    max_concurrent_runs = 1
   }
-
-  depends_on = [aws_glue_catalog_database.etl_db]
 }
 
-resource "aws_glue_crawler" "etl_crawler" {
-  name          = var.glue_crawler_name
+resource "aws_glue_crawler" "nyc_crawler" {
+  name          = "nyc-yellow-crawler"
   role          = local.glue_role_arn
-  database_name = aws_glue_catalog_database.etl_db.name
+  database_name = aws_glue_catalog_database.nyc_db.name
 
   s3_target {
-    path = "s3://${var.bucket_name_prefix}/cleaned_data/"
+    path = local.cleaned_data_s3_path
   }
 
-  depends_on = [aws_glue_job.etl_job]
+  schedule = "cron(0 12 * * ? *)"
 }
