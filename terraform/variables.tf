b2e80b999bb2c88f39847579b823b561339e4540
Gyandeep42 <yadavgyan2002@email.com>
2025-08-06 19:34:40 +0530

    commit after first changes after workingand making sore that data uploaded through .yml

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,32 +5,32 @@ variable "region" {
 
 # Common prefix (used as part of bucket names)
 variable "bucket_name_prefix" {
-  default = "opbkt-glue-rawonetwothreefour"
+  default = "opbkt-glue-rawautomate"
 }
 
 # Raw S3 bucket name
 variable "raw_bucket_name" {
-  default = "opbkt-glue-rawonetwothreefour"
+  default = "opbkt-glue-rawauto"
 }
 
 # Cleaned S3 bucket name
 variable "cleaned_bucket_name" {
-  default = "gpbkt-cleanedonetwothreefour"
+  default = "gpbkt-cleanedauto"
 }
 
 # Glue Catalog Database name
 variable "glue_db_name" {
-  default = "nyc-yellow-taxi-trip-data-dbonetwothreefour"
+  default = "nyc-yellow-taxi-trip-data-dbauto"
 }
 
 # Glue ETL Job name
 variable "glue_job_name" {
-  default = "firstjobonetwothreefour"
+  default = "firstjobauto"
 }
 
 # Glue Crawler name
 variable "glue_crawler_name" {
-  default = "nyc-crawler-onetwothreefour"
+  default = "nyc-crawler-auto"
 }
 
 # Path to the ETL script in S3
