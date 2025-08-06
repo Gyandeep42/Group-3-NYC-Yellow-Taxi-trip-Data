dd668308d3d68ae35d9ba0d8ebc78ae988b6f447
Gyandeep42 <yadavgyan2002@email.com>
2025-08-07 01:42:47 +0530

    from scrach commit

--- infra/variables.tf
+++ infra/variables.tf
@@ -10,6 +10,14 @@ variable "bucket_name_prefix" {
   default = "first-glue-bkt-grp-three-nyc"
 }
 
+variable "region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1" # or your desired region
+}
+
+
+
 variable "script_s3_path" {
   default = "s3://first-glue-bkt-grp-three-nyc/scripts/etl-glue-script.py"
 }
