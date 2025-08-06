b4b384a7fe23e4342d130ef56e227e05ee7419ba
Gyandeep42 <yadavgyan2002@email.com>
2025-08-06 13:34:12 +0530

    commit after first changes

--- terraform/script_upload.tf
+++ terraform/script_upload.tf
@@ -3,4 +3,7 @@ resource "aws_s3_object" "etl_script" {
   key    = var.etl_script_s3_key
   source = "${path.module}/../etl/etl-glue-script.py"
   etag   = filemd5("${path.module}/../etl/etl-glue-script.py")
+
+  # 👇 Ensure bucket is created before uploading
+  depends_on = [aws_s3_bucket.raw_bucket]
 }
