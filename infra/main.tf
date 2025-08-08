6c252790ecb8dbb92b7601f7f9b183d47c6ea9f0
Gyandeep42 <yadavgyan2002@email.com>
2025-08-08 18:21:23 +0530

    path changes

--- infra/main.tf
+++ infra/main.tf
@@ -32,7 +32,7 @@ resource "aws_glue_crawler" "etl_crawler" {
   database_name = aws_glue_catalog_database.etl_db.name
 
   s3_target {
-    path = "s3://${var.bucket_name_prefix}/transformeddata/"
+    path = "s3://raw-data-grp-3/cleaned data/transformeddata/"
   }
 
   depends_on = [aws_glue_job.etl_job]
