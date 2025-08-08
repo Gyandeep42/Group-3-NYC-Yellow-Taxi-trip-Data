fb86c66b3ad33e0c7ea6a4f61ab8b1c66190588f
Gyandeep42 <yadavgyan2002@email.com>
2025-08-08 13:51:17 +0530

    full automation

--- etl/etl-glue-script.py
+++ etl/etl-glue-script.py
@@ -5,7 +5,8 @@ from awsglue.context import GlueContext
 from awsglue.job import Job
 from pyspark.context import SparkContext
 from pyspark.sql import Window
-from pyspark.sql.functions import lit, col, when, expr, to_date, date_format, year, dayofmonth, floor, row_number, hour, rand
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import lit, col, when, expr, to_date, date_format, year, dayofmonth, floor, row_number, hour
 from pyspark.sql.types import IntegerType
 from awsglue.dynamicframe import DynamicFrame
 
@@ -17,56 +18,66 @@ spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 
-# Load datasets
-master_dyf = glueContext.create_dynamic_frame.from_options(
-    connection_type="s3",
-    connection_options={"paths": ["s3://data-lake-gr3/data-2019/part-00000-f3389a53-21c4-4db1-aa98-e96af46b9665-c000.snappy.parquet"]},
-    format="parquet"
-)
-
-zone_dyf = glueContext.create_dynamic_frame.from_options(
-    connection_type="s3",
-    connection_options={"paths": ["s3://nycfinalp/taxi_zone_lookup.csv"]},
-    format="csv",
-    format_options={"withHeader": True}
-)
-
-# Convert DynamicFrame to DataFrame and take a random sample of 50,000 rows
-master_df = master_dyf.toDF().orderBy(rand(seed=42)).limit(50000)
-zone_df = zone_dyf.toDF()
+master_df= spark.read.option("header", True).parquet("s3://datalake-grp-03/nyc-merged-raw-data/part-00000-a7fd998c-ac15-4237-b9b0-684f953b2375-c000.snappy.parquet")
+zone_df = spark.read.option("header", True).csv("s3://nycfinalp/taxi_zone_lookup.csv")
 
 # Data transformations
 transformed_df = (master_df
-    .filter((year("tpep_pickup_datetime") >= 2019) & (year("tpep_pickup_datetime") <= 2022))
-    .withColumn("tpep_pickup_datetime", col("tpep_pickup_datetime").cast("timestamp"))
-    .withColumn("week_of_month", (floor((dayofmonth("tpep_pickup_datetime") - 1) / 7) + 1).cast("int"))
-    .withColumn("day_name", date_format("tpep_pickup_datetime", "EEEE"))
+   # Filter out null & zero passenger_count and null & 99 RatecodeID
     .filter((col("passenger_count").isNotNull()) & (col("passenger_count") != 0))
+    .filter((col("RatecodeID").isNotNull()) & (col("RatecodeID") != 99))
+
+  # Drop unused columns
+    .drop("store_and_fwd_flag", "airport_fee", "congestion_surcharge")
+     
+    # Cast timestamp
+    .withColumn("tpep_pickup_datetime", col("tpep_pickup_datetime").cast("timestamp"))
+    
+    # Add week of month
+    .withColumn("week_of_month", (floor((dayofmonth("tpep_pickup_datetime") - 1) / 7) + 1).cast("int"))
+    
+    # Add day name
+    .withColumn("day_name", date_format("tpep_pickup_datetime", "EEEE"))
+    
+    # Add time of day
     .withColumn("time_of_day",
                 when((hour(col("tpep_pickup_datetime")) >= 6) & (hour(col("tpep_pickup_datetime")) < 18), "Day")
                 .otherwise("Night"))
-    .fillna({'RatecodeID': 99})
+    
+    # cast types
+
     .withColumn("passenger_count", col("passenger_count").cast(IntegerType()))
     .withColumn("RatecodeID", col("RatecodeID").cast(IntegerType()))
-    .drop("store_and_fwd_flag", "airport_fee", "congestion_surcharge")
+    
+    # Join with zone lookup tables
     .join(zone_df.select(col("LocationID").alias("PULocationID"),
-                         col("Zone").alias("pickup_zone"),
-                         col("Borough").alias("pickup_borough")),
+                        col("Zone").alias("pickup_zone"),
+                        col("Borough").alias("pickup_borough")),
           on="PULocationID", how="left")
     .join(zone_df.select(col("LocationID").alias("DOLocationID"),
-                         col("Zone").alias("dropoff_zone"),
-                         col("Borough").alias("dropoff_borough")),
+                        col("Zone").alias("dropoff_zone"),
+                        col("Borough").alias("dropoff_borough")),
           on="DOLocationID", how="left")
+    
+    # Add row number as Id
     .withColumn("Id", row_number().over(Window.orderBy(lit(1))))
+    
+    # Add tip percentage
     .withColumn("tip_percentage",
                 when(col("fare_amount") > 0, (col("tip_amount") / col("fare_amount")) * 100)
                 .otherwise(0))
+    
+    # Filter extreme values
     .filter((col("trip_distance") <= 100) & (col("fare_amount") <= 500))
+    
+    # Add distance bucket
     .withColumn("distance_bucket",
                 when(col("trip_distance") < 1, "0-1 miles")
                 .when((col("trip_distance") >= 1) & (col("trip_distance") < 5), "1-5 miles")
                 .when((col("trip_distance") >= 5) & (col("trip_distance") < 10), "5-10 miles")
                 .otherwise("10+ miles"))
+    
+    # Map payment type
     .withColumn("payment_type_desc",
                 expr("""
                     CASE payment_type
@@ -79,6 +90,8 @@ transformed_df = (master_df
                         ELSE 'Other'
                     END
                 """))
+    
+    # Map RatecodeID
     .withColumn("ratecode_desc",
                 when(col("RatecodeID") == 1, "Standard rate")
                 .when(col("RatecodeID") == 2, "JFK")
@@ -86,32 +99,21 @@ transformed_df = (master_df
                 .when(col("RatecodeID") == 4, "Nassau or Westchester")
                 .when(col("RatecodeID") == 5, "Negotiated fare")
                 .when(col("RatecodeID") == 6, "Group ride")
-                .when(col("RatecodeID") == 99, "Unknown")
                 .otherwise("Other"))
+    
+    # Map VendorID
     .withColumn("vendor_desc",
                 when(col("VendorID") == 1, "Creative Mobile Technologies, LLC")
                 .when(col("VendorID") == 2, "Curb Mobility, LLC")
                 .when(col("VendorID") == 6, "Myle Technologies Inc")
                 .when(col("VendorID") == 7, "Helix")
                 .when(col("VendorID").isin(3, 4, 5), "Third Party"))
+    
+    # Drop unnecessary columns
     .drop("service_zone")
 )
 
-# Convert DataFrame to DynamicFrame
-sample_dyf = DynamicFrame.fromDF(transformed_df, glueContext, "sample_dyf")
+transformed_df.write.mode("overwrite").partitionBy("year").parquet("")
 
-# Write partitioned output to S3 by year
-glueContext.write_dynamic_frame.from_options(
-    frame=sample_dyf,
-    connection_type="s3",
-    connection_options={
-        "path": "s3://second-glue-bkt-grp-three-nyc/testing-master-data/",
-        "partitionKeys": ["year"]
-    },
-    format="parquet",
-    transformation_ctx="write_output",
-    format_options={"compression": "snappy"}
-)
-
-# Commit the job 
+# Commit the job
 job.commit()
