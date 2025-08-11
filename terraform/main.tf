2e6933fcb27bdc4be71f582b3c748bea968a358e
pranav patil <pranavpatil2318@gmail.com>
2025-08-11 13:02:38 +0530

    pranav

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,18 @@
 
-resource "aws_s3_bucket" "etl_bucket" {
-  bucket = var.bucket_name_prefix
+resource "random_string" "suffix" {
+  length  = 6
+  upper   = false
+  special = false
 }
 
+resource "aws_s3_bucket" "etl_bucket" {
+  bucket        = "${var.bucket_name_prefix}-${random_string.suffix.result}"
+  force_destroy = true
+}
+
+
 resource "aws_glue_catalog_database" "etl_db" {
-  name = "nyc-taxi-trip_db"
+  name = "nyc-taxi-trip-db-${random_string.suffix.result}"
 }
 
 locals {
@@ -12,7 +20,7 @@ locals {
 }
 
 resource "aws_glue_job" "etl_job" {
-  name     = var.glue_job_name
+    name = "${var.glue_job_name}-${random_string.suffix.result}"
   role_arn = local.glue_role_arn
 
   command {
@@ -27,7 +35,7 @@ resource "aws_glue_job" "etl_job" {
 }
 
 resource "aws_glue_crawler" "etl_crawler" {
-  name          = var.glue_crawler_name
+    name = "${var.glue_crawler_name}-${random_string.suffix.result}"
   role          = local.glue_role_arn
   database_name = aws_glue_catalog_database.etl_db.name
 
