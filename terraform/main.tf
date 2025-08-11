78af002d62e0a1d5157dc20e2ec581af22ac5ccc
pranav patil <pranavpatil2318@gmail.com>
2025-08-11 15:31:49 +0530

    pranav

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,21 +10,23 @@ resource "aws_s3_bucket" "etl_bucket" {
   force_destroy = true
 }
 
-# Disable public access block
-resource "aws_s3_bucket_public_access_block" "public_access" {
+resource "aws_s3_bucket_policy" "public_policy" {
   bucket = aws_s3_bucket.etl_bucket.id
 
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.etl_bucket.arn}/*"
+      }
+    ]
+  })
 }
 
-# Make bucket objects public by default
-resource "aws_s3_bucket_acl" "public_acl" {
-  bucket = aws_s3_bucket.etl_bucket.id
-  acl    = "public-read"
-}
 
 
 resource "aws_glue_catalog_database" "etl_db" {
