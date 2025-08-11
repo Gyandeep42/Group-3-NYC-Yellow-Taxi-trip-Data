012b248f2ed96ec44ae4c4eff153747c4f90f784
pranav patil <pranavpatil2318@gmail.com>
2025-08-11 17:59:05 +0530

    pranav

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,15 +1,19 @@
-
 resource "random_string" "suffix" {
   length  = 6
   upper   = false
   special = false
 }
 
-
-
 resource "aws_s3_bucket" "etl_bucket" {
-  bucket        = "${var.bucket_name_prefix}-${random_string.suffix.result}"
-  force_destroy = true
+  bucket            = "${var.bucket_name_prefix}-${random_string.suffix.result}"
+  force_destroy     = true
+  object_ownership  = "ObjectWriter" # ✅ Enable ACLs
+}
+
+# Set ACL for the bucket
+resource "aws_s3_bucket_acl" "etl_bucket_acl" {
+  bucket = aws_s3_bucket.etl_bucket.id
+  acl    = "private" # Can change to "public-read" if needed
 }
 
 # Disable Block Public Access so we can apply a public policy
@@ -42,8 +46,6 @@ resource "aws_s3_bucket_policy" "public_policy" {
   depends_on = [aws_s3_bucket_public_access_block.disable_block]
 }
 
-
-
 resource "aws_glue_catalog_database" "etl_db" {
   name = "nyc-taxi-trip-db-${random_string.suffix.result}"
 }
@@ -55,13 +57,13 @@ locals {
 resource "aws_s3_object" "glue_script" {
   bucket = aws_s3_bucket.etl_bucket.bucket
   key    = "scripts/etl-glue-script.py"
-  source = "${path.module}/../etl/etl-glue-script.py" # Adjust path if needed
+  source = "${path.module}/../etl/etl-glue-script.py"
   etag   = filemd5("${path.module}/../etl/etl-glue-script.py")
 }
 
 resource "aws_glue_job" "etl_job" {
-    name = "${var.glue_job_name}-${random_string.suffix.result}"
-    role_arn = local.glue_role_arn
+  name     = "${var.glue_job_name}-${random_string.suffix.result}"
+  role_arn = local.glue_role_arn
 
   command {
     name            = "glueetl"
@@ -72,22 +74,17 @@ resource "aws_glue_job" "etl_job" {
   glue_version      = "4.0"
   number_of_workers = 2
   worker_type       = "G.1X"
-  depends_on = [aws_s3_object.glue_script]
+  depends_on        = [aws_s3_object.glue_script]
 }
 
-
-
-
 resource "aws_glue_crawler" "etl_crawler" {
-    name = "${var.glue_crawler_name}-${random_string.suffix.result}"
-    role= local.glue_role_arn
-    database_name = aws_glue_catalog_database.etl_db.name
+  name          = "${var.glue_crawler_name}-${random_string.suffix.result}"
+  role          = local.glue_role_arn
+  database_name = aws_glue_catalog_database.etl_db.name
 
   s3_target {
     path = "s3://inputdata-bucket-test/cleaned-data/transformeddata/"
   }
 
-
   depends_on = [aws_glue_job.etl_job]
 }
-#jlihoiuifawfi
