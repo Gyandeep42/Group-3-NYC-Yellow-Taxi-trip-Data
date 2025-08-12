6a9d51ff19489e25a7a8079166b67a5c244d9da8
pranav patil <pranavpatil2318@gmail.com>
2025-08-12 13:51:40 +0530

    pranav

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,50 +1,80 @@
-# ===== Variables =====
-variable "region" {
-  default = "us-east-1"
+# ===============================
+# Provider Configuration
+# ===============================
+provider "aws" {
+  region = var.region
 }
 
-variable "etl_bucket_name" {
-  default = "third-glue-bkt-grp-three-nyc" # Permanent bucket
+# ===============================
+# Variables
+# ===============================
+variable "region" {
+  description = "AWS region to deploy resources in"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "glue_db_name" {
+  description = "Name of the Glue database"
+  type        = string
+  default     = "my_glue_db"
 }
 
 variable "glue_job_name" {
-  default = "glue-etl-job"
+  description = "Name of the Glue ETL job"
+  type        = string
+  default     = "glue-etl-job"
 }
 
 variable "glue_crawler_name" {
-  default = "my-etl-crawler"
+  description = "Name of the Glue crawler"
+  type        = string
+  default     = "my-etl-crawler"
 }
 
-# ===== Data Source: Existing Bucket =====
-data "aws_s3_bucket" "etl_bucket" {
-  bucket = var.etl_bucket_name
+variable "script_file" {
+  description = "Local path to Glue ETL script"
+  type        = string
+  default     = "etl/etl-glue-script.py"
 }
 
-# ===== Local Variables =====
+# ===============================
+# Locals
+# ===============================
 locals {
-  glue_role_arn   = "arn:aws:iam::914016866997:role/LabRole"
-  run_folder      = formatdate("YYYYMMDD-HHmmss", timestamp())
-  script_s3_path  = "s3://${var.etl_bucket_name}/scripts/${local.run_folder}/etl-glue-script.py"
-  transformed_path = "s3://${var.etl_bucket_name}/cleaned-data/${local.run_folder}/"
+  script_s3_path = "s3://${aws_s3_bucket.etl_scripts.bucket}/scripts/${basename(var.script_file)}"
 }
 
-# ===== Glue Database =====
-resource "aws_glue_catalog_database" "etl_db" {
-  name = "nyc-taxi-trip-db"
+# ===============================
+# S3 Bucket for Scripts
+# ===============================
+resource "aws_s3_bucket" "etl_scripts" {
+  bucket = "etl-scripts-${random_id.bucket_suffix.hex}"
 }
 
-# ===== Upload Glue Script =====
+resource "random_id" "bucket_suffix" {
+  byte_length = 4
+}
+
 resource "aws_s3_object" "glue_script" {
-  bucket = var.etl_bucket_name
-  key    = "scripts/${local.run_folder}/etl-glue-script.py"
-  source = "${path.module}/../etl/etl-glue-script.py"
-  etag   = filemd5("${path.module}/../etl/etl-glue-script.py")
+  bucket = aws_s3_bucket.etl_scripts.bucket
+  key    = "scripts/${basename(var.script_file)}"
+  source = var.script_file
 }
 
-# ===== Glue Job =====
-resource "aws_glue_job" "etl_job" {
+# ===============================
+# Glue Database
+# ===============================
+resource "aws_glue_catalog_database" "this" {
+  name = var.glue_db_name
+}
+
+# ===============================
+# Glue Job
+# ===============================
+resource "aws_glue_job" "this" {
   name     = var.glue_job_name
-  role_arn = local.glue_role_arn
+  role_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/LabRole"
 
   command {
     name            = "glueetl"
@@ -52,34 +82,45 @@ resource "aws_glue_job" "etl_job" {
     python_version  = "3"
   }
 
-  glue_version      = "4.0"
-  number_of_workers = 2
-  worker_type       = "G.1X"
-  depends_on        = [aws_s3_object.glue_script]
-}
-
-# ===== Glue Crawler =====
-resource "aws_glue_crawler" "etl_crawler" {
-  name          = var.glue_crawler_name
-  role          = local.glue_role_arn
-  database_name = aws_glue_catalog_database.etl_db.name
-
-  s3_target {
-    path = local.transformed_path
+  default_arguments = {
+    "--TempDir" = "s3://${aws_s3_bucket.etl_scripts.bucket}/temp/"
   }
 
-  depends_on = [aws_glue_job.etl_job]
+  glue_version = "4.0"
+  max_capacity = 2
 }
 
-# ===== Outputs =====
+# ===============================
+# Glue Crawler
+# ===============================
+resource "aws_glue_crawler" "this" {
+  name          = var.glue_crawler_name
+  role          = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/LabRole"
+  database_name = aws_glue_catalog_database.this.name
+
+  s3_target {
+    path = "s3://${aws_s3_bucket.etl_scripts.bucket}/output/"
+  }
+
+  schedule = "cron(0 12 * * ? *)"
+}
+
+# ===============================
+# Data Sources
+# ===============================
+data "aws_caller_identity" "current" {}
+
+# ===============================
+# Outputs
+# ===============================
 output "etl_bucket_name" {
-  value = var.etl_bucket_name
+  value = aws_s3_bucket.etl_scripts.bucket
 }
 
 output "glue_job_name" {
-  value = aws_glue_job.etl_job.name
+  value = aws_glue_job.this.name
 }
 
 output "glue_crawler_name" {
-  value = aws_glue_crawler.etl_crawler.name
+  value = aws_glue_crawler.this.name
 }
