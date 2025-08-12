126b4b434f514d4bff7cbbc05ec9ceb1dd1628cb
Gyandeep42 <yadavgyan2002@email.com>
2025-08-12 19:46:19 +0530

    final commit from gyandeep

--- infra/main.tf
+++ infra/main.tf
@@ -1,23 +1,35 @@
-
-resource "aws_s3_bucket" "etl_bucket" {
-  bucket = var.bucket_name_prefix
-}
-
-resource "aws_glue_catalog_database" "etl_db" {
-  name = "nyc-taxi-trip_db"
-}
-
 locals {
+  # Hardcoded bucket name
+  etl_bucket_name = "third-glue-bkt-grp-three-nyc"
+
+  # Generate timestamp
+  timestamp = formatdate("YYYYMMDDhhmmss", timestamp())
+
+  # Auto-generated names for resources
+  glue_job_name      = "nyc-yellow-taxi-data-${local.timestamp}"
+  glue_db_name       = "nyc-yellow-taxi-data-${local.timestamp}-db"
+  glue_crawler_name  = "nyc-yellow-taxi-data-${local.timestamp}-crawler"
+
   glue_role_arn = "arn:aws:iam::963702399712:role/LabRole"
 }
 
+# Create S3 bucket only if it doesn't exist
+resource "aws_s3_bucket" "etl_bucket" {
+  bucket = local.etl_bucket_name
+}
+
+resource "aws_glue_catalog_database" "etl_db" {
+  name = local.glue_db_name
+}
+
 resource "aws_glue_job" "etl_job" {
-  name     = var.glue_job_name
+  name     = local.glue_job_name
   role_arn = local.glue_role_arn
 
   command {
     name            = "glueetl"
-    script_location = var.script_s3_path
+    # Script path will be handled in GitHub Actions logic
+    script_location = "s3://${local.etl_bucket_name}/scripts/latest/etl-glue-script.py"
     python_version  = "3"
   }
 
@@ -27,7 +39,7 @@ resource "aws_glue_job" "etl_job" {
 }
 
 resource "aws_glue_crawler" "etl_crawler" {
-  name          = var.glue_crawler_name
+  name          = local.glue_crawler_name
   role          = local.glue_role_arn
   database_name = aws_glue_catalog_database.etl_db.name
 
